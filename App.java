f72ec85cde56c93cf1fbd07ae6c7a8d8b4dde45c
ridasoal <ridasoal@gmail.com>
2022-12-14 11:00:58 -0300

    subindo

--- App.java
+++ App.java
@@ -5,8 +5,7 @@ public class App {
   
     public static void main(String[] args) {
       boolean abate = false;
-      int indice = 0; //para controlar qual individuo do arraylist está
-      ArrayList<Animais> animal = new ArrayList<Animais>();
+          ArrayList<Animais> animal = new ArrayList<Animais>();
     
     try (Scanner scan = new Scanner(System.in)) {
         int teclado = 0;
@@ -26,17 +25,30 @@ public class App {
   while(abate == true){
   System.out.println("Digite outro comando(número): ");
   teclado = scan.nextInt();
-  switch (teclado){//2 aumenta numero de animais
-    case 2: animal.add(new Animais());System.out.println(indice); indice++; System.out.println(indice); 
-  break;
-  
+  switch (teclado){
+    //2 aumenta numero de animais
+    case 2: animal.add(new Animais());System.out.println("Caiu animal número: " + animal.get(animal.size()-1));
+    animal.get(animal.size()-1).setNumInsensibilizacao();
+    break;
+   
 //3 aumenta numer0 de insensibilizações
-    case 3:  animal.get(animal.size()-1).setNumInsensibilizacao(1); System.out.println(indice);
+    case 3:  animal.get(animal.size()-1).setNumInsensibilizacao();
+    System.out.println(animal.get(animal.size()-1));
   break;
   
 //4 houve sinais muda pra true = ruim 
-    case 4: animal.get(animal.size()-1).setHouveSinaisObservadosNoAnimal(); System.out.println("\n  houve sinais:  " + animal.get(animal.size()-1).isHouveSinaisObservadosNoAnimal());
+    case 4: animal.get(animal.size()-1).setHouveSinaisObservadosNoAnimal(); 
+    System.out.println("\n  houve sinais:  " + animal.get(animal.size()-1).isHouveSinaisObservadosNoAnimal());
     break;
+
+//5 vocalizacao
+    case 5: animal.get(animal.size()-1).setVocalizacao(true);
+    System.out.println("\n Houve vocalização no animal: " + animal.get(animal.size()-1).isVocalizacao());
+    break;
+
+//6 respiração 
+    case 6: animal.get(animal.size()-1).setRespiracao(true);
+    System.out.println("\n Houve respiração no animal: " + animal.get(animal.size()-1).isRespiracao()); 
   //parar abate
     case 0: abate = false; break;
 
@@ -46,9 +58,21 @@ public class App {
   }
 
 System.out.println(teclado);
-System.out.println("animais " + animal.size());
-System.out.println("número de insensibilizações do animal " + animal.get(animal.size()).getNumInsensibilizacao());
-System.out.println("houve sinais: " + animal.get(animal.size()-1).isHouveSinaisObservadosNoAnimal());
+System.out.println("Número de animais " + animal.size());
+
+//saída info animais
+for (Animais animais : animal) { //arrumar o tostring
+  System.out.println("número de insensibilizações do animal " +animais+ (animais).getNumInsensibilizacao());
+  //System.out.println("\n");
+  System.out.println(" houve sinais: " + animais + animais.isHouveSinaisObservadosNoAnimal());
+ // System.out.println("\n");
+  System.out.println("Houve vocalização no animal: " + animais + animais.isHouveSinaisObservadosNoAnimal());
+ // System.out.println("\n");
+  System.out.println("Houve respiração no animal: " + animais + animais.isRespiracao());
+
+}
+
+
     }
 
  System.out.println("Abate: " + abate);
